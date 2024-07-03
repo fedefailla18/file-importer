9c200cec0f29a8f5cc1dcc513c15a51812879797
Facundo Martinez <facundo.martinez89@gmail.com>
2024-07-03 14:56:15 -0400

    001 add script start db (#12)
    * Delete version path to prevent warning
    
    * Add script to start the database
    
    * Modify script to correctly create the schema

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,18 +1,18 @@
-
--- Create a new database with the user as the owner
 CREATE DATABASE importer_database OWNER root;
--- Create a new schema and set the user as the owner
+
+GRANT ALL PRIVILEGES ON DATABASE importer_database TO root;
+
+\connect importer_database
+
 CREATE SCHEMA file_importer_schema AUTHORIZATION root;
 
--- Create the event table inside the new schema
 BEGIN;
-CREATE TABLE IF NOT EXISTS
-    file_importer_schema.event (
-        id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
-          aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
-          event_data JSON NOT NULL,
-          version INT,
-          UNIQUE(aggregate_id, version)
+CREATE TABLE IF NOT EXISTS file_importer_schema.event (
+    id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
+    aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
+    event_data JSON NOT NULL,
+    version INT,
+    UNIQUE(aggregate_id, version)
 );
-CREATE INDEX idx_event_aggregate_id ON file_importer_schema.event (aggregate_id);
+CREATE INDEX idx_importer_aggregate_id ON file_importer_schema.event (aggregate_id);
 COMMIT;
